9f46abcd7a4797db6f56d898b86481f20c0d1a3f
Shymanska_Maria <shymanskam524@gmail.com>
2024-10-07 21:20:01 +0200

    update css

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -6,26 +6,68 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 
 
+const date = document.querySelector('#datetime-picker');
+const btn = document.querySelector('[data-start]');
+const day = document.querySelector('[data-days]');
+const hour = document.querySelector('[data-hours]');
+const min = document.querySelector('[data-minutes]');
+const sec = document.querySelector('[data-seconds]');
+const spans = document.querySelectorAll('.value');
+
+
+
+
+
+
+
+
+
+function onStart() {
+  datepicker.element.disabled = true; // Вимикаємо вибір дати
+  const intervalId = setInterval(() => {
+    const curDate = new Date();
+    let diff = selectedDate - curDate; // Перераховуємо різницю кожного разу
+
+    if (diff <= 0) {
+      clearInterval(intervalId); // Зупиняємо таймер, коли час спливає
+      updateTimer(convertMs(0)); // Обнуляємо таймер
+      return;
+    }
+
+    const convertedData = convertMs(diff); // Конвертуємо час
+    updateTimer(convertedData); // Оновлюємо таймер на екрані
+  }, 1000);
+}
+
+
+
+
+
+
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minutesEl.textContent = minutes;
+  secondsEl.textContent = seconds;
+}
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor(
+    (((ms % day) % hour) % minute) / second
+  ));
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
